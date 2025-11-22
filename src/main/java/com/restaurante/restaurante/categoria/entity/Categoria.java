55e0bf32a22ada3d2d35e36eebfbbd84b60d1440
laszlocaballero <laszlocaballero@gmail.com>
2025-11-22 01:13:19 -0500

    feat: Añadir campo descripcion en Categoria y Comida, y definir su tipo como TEXT

--- src/main/java/com/restaurante/restaurante/categoria/entity/Categoria.java
+++ src/main/java/com/restaurante/restaurante/categoria/entity/Categoria.java
@@ -6,6 +6,7 @@ import com.restaurante.restaurante.comida.entity.Comida;
 import com.restaurante.restaurante.recursos.entity.Recurso;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Categoria {
 
     private String nombre;
 
+    @Column(columnDefinition = "TEXT")
     private String descripcion;
 
     @ManyToMany(mappedBy = "categorias", cascade = CascadeType.ALL)
--- src/main/java/com/restaurante/restaurante/comida/entity/Comida.java
+++ src/main/java/com/restaurante/restaurante/comida/entity/Comida.java
@@ -7,6 +7,7 @@ import com.restaurante.restaurante.pedido.entity.PedidoComida;
 import com.restaurante.restaurante.recursos.entity.Recurso;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -39,6 +40,7 @@ public class Comida {
 
     private String slug;
 
+    @Column(columnDefinition = "TEXT")
     private String descripcion;
 
     private Double precio;
